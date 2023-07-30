f1e0a4dc6c1972c84ab3790765d3e9a89636fc01
taboun1234 <118006245+taboun1234@users.noreply.github.com>
2023-07-30 16:25:29 +0200

    Add files via upload

---
+++ lib/RTC/RTC.cpp
@@ -0,0 +1,13 @@
+#include "RTC.h"
+
+void setup() {
+ /*
+  setup_default_uart();
+  printf("Hello, !\n");
+  bi_decl(bi_program_description("First Blink"));
+  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);//*/
+
+}
---
+++ lib/RTC/RTC.h
@@ -0,0 +1,11 @@
+#ifndef __RTC_h_
+#define __RTC_h_
+
+#pragma once
+#include <stdio.h>
+
+const uint LED_PIN = 25;
+void setup();
+
+
+#endif
